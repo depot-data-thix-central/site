6524d33cb03c93c07df21436e448f0d0283d5756
depot-data-thix-central <datathix@gmail.com>
2026-09-15 09:55:08 +0300

    Update app_test.dart

--- test/app_test.dart
+++ test/app_test.dart
@@ -7,20 +7,30 @@ void main() {
       expect(Validators.validateEmail('nathan@sonathix.group'), isNull);
       expect(Validators.validateEmail('a.b+c@d.co'), isNull);
     });
+
     test('rejette les e-mails invalides', () {
       expect(Validators.validateEmail(''), isNotNull);
       expect(Validators.validateEmail('pas-un-email'), isNotNull);
       expect(Validators.validateEmail('a@b'), isNotNull);
+      expect(Validators.validateEmail('x@y.${'a' * 64}'), isNotNull);
     });
   });
 
   group('Validators.sanitizeInput', () {
     test('supprime caractères de contrôle et espaces multiples', () {
-      expect(Validators.sanitizeInput('a�bc'), 'abc');
+      // ⚠️ Caractères de contrôle en échappements Unicode (copier-coller sûr)
+      expect(Validators.sanitizeInput('a\u0000b\u0007c\u001B'), 'abc');
+      expect(Validators.sanitizeInput('bonjour\u0009monde\u000D!'), 'bonjourmonde!');
       expect(Validators.sanitizeInput('  multi   espaces '), 'multi espaces');
     });
+
     test('tronque à maxLength', () {
       expect(Validators.sanitizeInput('a' * 300, maxLength: 10).length, 10);
+    });
+
+    test('conserve les caractères accentués et la ponctuation', () {
+      expect(Validators.sanitizeInput('  Afrique & sécurité  '),
+          'Afrique & sécurité');
     });
   });
 }

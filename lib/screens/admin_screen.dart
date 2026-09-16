d8f7663818a8a946bc203995e81c32e1600524b3
depot-data-thix-central <datathix@gmail.com>
2026-09-16 15:44:11 +0300

    Remove unused import for dart:typed_data

--- lib/screens/admin_screen.dart
+++ lib/screens/admin_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'dart:js_interop';
-import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
